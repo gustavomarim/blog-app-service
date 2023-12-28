c59250462aafe523e6f037bbf007c82a386b8ddf
gustavomarim <gustavo.dantas.marim@outlook.com>
2023-12-28 19:15:17 -0300

    refactor: remove validação de usuário administrativo

--- src/routes/admin.ts
+++ src/routes/admin.ts
@@ -1,22 +1,21 @@
-import { Router } from 'express';
-import CategoryController from '../controllers/CategoryController';
-import PostController from '../controllers/PostController';
-import { isAdmin } from './../helpers/isAdmin';
+import { Router } from "express";
+import CategoryController from "../controllers/CategoryController";
+import PostController from "../controllers/PostController";
 const router = Router();
 
 const categoryController = CategoryController;
 const postController = PostController;
 
 // Rota Category
-router.get('/admin/categories', isAdmin, categoryController.read);
-router.post('/admin/categories', isAdmin, categoryController.create);
-router.put('/admin/categories/:id', isAdmin, categoryController.update);
-router.delete('/admin/categories/:id', isAdmin, categoryController.delete);
+router.get("/admin/categories", categoryController.read);
+router.post("/admin/categories", categoryController.create);
+router.put("/admin/categories/:id", categoryController.update);
+router.delete("/admin/categories/:id", categoryController.delete);
 
 // Rota Post
-router.get('/admin/posts', isAdmin, postController.read);
-router.post('/admin/posts', isAdmin, postController.create);
-router.put('/admin/posts/:id', isAdmin, postController.update);
-router.delete('/admin/posts/:id', isAdmin, postController.delete);
+router.get("/admin/posts", postController.read);
+router.post("/admin/posts", postController.create);
+router.put("/admin/posts/:id", postController.update);
+router.delete("/admin/posts/:id", postController.delete);
 
 module.exports = router;
