341ed205eaae81ba859b67e0ef6aa12941dd4752
gustavomarim <gustavo.dantas.marim@outlook.com>
2024-01-08 08:46:22 -0300

    feat(controllers): add tratamento de erro em logout()

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -4,10 +4,8 @@ const router = Router();
 
 const userController = UserController;
 
-// Rota Register
 router.post("/users/register", userController.register);
 
-// Rota Login
 router.post("/users/login", userController.login);
 router.get("/users/logout", userController.logout);
 
