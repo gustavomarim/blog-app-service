2f546615336b1a235c11a101304713e3dc0ec274
Gustavo Dantas <gustavo.dantas.marim@outlook.com>
2025-07-11 22:20:22 -0300

    refactor(auth): mover lógica de verificação de administrador para AuthorizationService e remover middleware isAdmin

--- src/routes/admin.ts
+++ src/routes/admin.ts
@@ -1,60 +1,33 @@
-// // ./routes/admin.ts
-// import { Router } from "express";
-// import CategoryController from "../controllers/CategoryController";
-// import PostController from "../controllers/PostController";
-// import { isAdmin } from "../helpers/isAdmin";
-
-import { Router } from "express";
+import { Request, Response, Router } from "express";
 import { CategoryController } from "../controllers/CategoryController";
-import { PostController } from "../controllers/PostController";
-import { isAdmin } from "../helpers/isAdmin";
-
-// const router = Router();
-// const categoryController = CategoryController;
-// const postController = PostController;
-
-// // Rota Category
-// router.get("/admin/categories", isAdmin, categoryController.readAllCategories);
-// router.post("/admin/categories", isAdmin, categoryController.createCategory);
-// router.get(
-//   "/admin/categories/:id",
-//   isAdmin,
-//   categoryController.readCategoryById
-// );
-// router.put("/admin/categories/:id", isAdmin, categoryController.updateCategory);
-// router.delete(
-//   "/admin/categories/:id",
-//   isAdmin,
-//   categoryController.deleteCategory
-// );
-
-// // Rota Post
-// router.get("/admin/posts", isAdmin, postController.read);
-// router.get("/admin/posts/:id", isAdmin, postController.getPostById);
-// router.post("/admin/posts", isAdmin, postController.create);
-// router.put("/admin/posts/:id", isAdmin, postController.update);
-// router.delete("/admin/posts/:id", isAdmin, postController.delete);
-
-// export default router;
+import AuthorizationService from "../services/AuthorizationService";
 
 export class AdminRoutes {
   private router: Router;
   private categoryController: CategoryController;
-  private postController: PostController;
+  // private postController: PostController;
 
   constructor() {
     this.router = Router();
     this.categoryController = new CategoryController();
-    this.postController = new PostController();
+    // this.postController = new PostController();
     this.initializeRoutes();
   }
 
   private initializeRoutes() {
     this.router.get(
       "/categories",
-      isAdmin,
-      this.categoryController.getAllCategories.bind(this)
+      AuthorizationService.isAdmin(),
+      this.getAllCategories.bind(this)
     );
+  }
+
+  private async getAllCategories(request: Request, response: Response) {
+    try {
+      return await this.categoryController.getAllCategories(request, response);
+    } catch (error) {
+      console.error(error, "Erro ao buscar todas as categorias");
+    }
   }
 
   public getRouter() {
